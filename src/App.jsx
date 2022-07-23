ba270097329835bfc1976b753b7f63c48cb73bb8
mokyeah <mzmmzm2008@gmail.com>
2022-07-23 17:39:29 +0800

    Food
    food

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import food from "./assets/food.jpg";
+function App() {
+  return (
+    <div className="bg-gradient-to-br from-green-300 flex justify-center items-center w-full h-screen ">
+      <div className="rounded-xl overflow-hidden">
+        <img src={food} className="w-96" />
+        <div className="bg-white p-5">
+          <div classname="text-md font-seimifold">Nasi Lemak</div>
+          <div>
+            by<span classname="font-semifold">me</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
